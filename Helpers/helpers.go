f95444c79edd8370696aed2fd7b09167080e4528
Boluwatife Ayodele <waplurd1@gmail.com>
2021-05-04 00:51:12 +0100

    added function for password hashing and salting

--- Helpers/helpers.go
+++ Helpers/helpers.go
@@ -1,7 +1,18 @@
 package Helpers
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
 func HandleError (err error){
 	if err != nil {
 		panic(err.Error())
 	}
+}
+
+func HashAndPassword(password []byte) string {
+	 hashed,err := bcrypt.GenerateFromPassword(password, bcrypt.MinCost) 
+	 HandleError(err)
+	 return string(hashed)
+	 
 }
